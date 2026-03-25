97795f3d52f24d1086c7c9d92af994d02dcf786c
Ciro <67671434+cirolpz@users.noreply.github.com>
2026-03-25 00:15:18 -0300

    <feat>(EnvioService)Creacion de crear, obtener, y buscar envío

---
+++ src/main/java/com/logitrack/logitrack_api/service/EnvioService.java
@@ -0,0 +1,33 @@
+package com.logitrack.logitrack_api.service;
+
+import com.logitrack.logitrack_api.model.Envio;
+import com.logitrack.logitrack_api.repository.EnvioRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class EnvioService {
+    private final EnvioRepository repository;
+
+    public EnvioService(EnvioRepository repository) {
+        this.repository = repository;
+    }
+
+    public Envio crearEnvio(Envio envio) {
+        envio.setTrackingId(UUID.randomUUID().toString());
+        envio.setEstado("Registrado");
+
+        return repository.save(envio);
+    }
+
+    public List<Envio> obtenerTodos() {
+        return repository.findAll();
+    }
+
+    public Envio getEnvioByTrackingId(String trackingId) {
+        return repository.findByTrackingId(trackingId)
+                .orElseThrow(() -> new RuntimeException("Envío no encontrado"));
+    }
+}
